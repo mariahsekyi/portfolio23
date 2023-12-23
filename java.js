e114610a59f2d938ef49036514ce4d9b1fe0622d
mariahsekyi <mariahsekyi@wustl.edu>
2023-12-22 20:02:35 -0600

    home page big changes

---
+++ java.js
@@ -0,0 +1,23 @@
+var i=0;
+var txt='Hi! My name is Maria Sekyi';
+var speed = 90;
+
+function typing(){
+   if (i < txt.length){
+        document.getElementById("hi").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typing, speed);
+    }
+}
+typing();
+
+var j=0;
+var txt1="I am a Human-Centered UX Researcher and Designer. This is my portfolio.";
+function typing1(){
+    if (j<txt1.length){
+        document.getElementById('me').innerHTML += txt1.charAt(j);
+        j++;
+        setTimeout(typing1, speed);
+    }
+}
+typing1();
